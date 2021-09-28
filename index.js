86875f00b5d7dc10eff27f14037c704a3b0dfd7b
AswathyVNair <88492274+AswathyVNair@users.noreply.github.com>
2021-09-28 18:01:11 +0530

    typecasting and processing

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
 
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
   hideMessage();
   if (billAmount.value > 0) {
-    if (cashGiven.value >= billAmount.value) {
-      const amountToBeReturned = cashGiven.value - billAmount.value;
+    if (Number(cashGiven.value) >= Number(billAmount.value)) {
+      const amountToBeReturned =
+        Number(cashGiven.value) - Number(billAmount.value);
+      calculateChange(amountToBeReturned);
     } else {
       showMessage(
         "The cash provided should atleast be equal to the bill amount"
@@ -25,4 +29,18 @@ function hideMessage() {
 function showMessage(messageGiven) {
   message.style.display = "block";
   message.innerText = messageGiven;
+}
+
+function calculateChange(amountToBeReturned) {
+  if (amountToBeReturned == 0) {
+    showMessage("No cash to return");
+  } else {
+    for (let i = 0; i < availableNotes.length; i++) {
+      const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+
+      amountToBeReturned = amountToBeReturned % availableNotes[i];
+      noOfNotes[i].innerText = numberOfNotes;
+      console.log(numberOfNotes);
+    }
+  }
 }
