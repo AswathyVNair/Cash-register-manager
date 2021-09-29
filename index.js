85b22ca6e933e1a76270f672f7b8c44a70dadb84
AswathyVNair <88492274+AswathyVNair@users.noreply.github.com>
2021-09-30 01:44:27 +0530

    feat : next button

--- index.js
+++ index.js
@@ -3,11 +3,23 @@ const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
+const visibility = document.querySelector("#input-visibility");
+const nextButton = document.querySelector("#next-button");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
-checkButton.addEventListener("click", function validateBillAndCashAmount() {
+visibility.style.display = "none";
+nextButton.addEventListener("click", function validateBillAmount() {
   hideMessage();
   if (billAmount.value > 0) {
+    visibility.style.display = "block";
+  } else {
+    showMessage("Invalid  Bill Amount");
+  }
+});
+
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
+  hideMessage();
+  if (Number(billAmount.value) > 0) {
     if (Number(cashGiven.value) >= Number(billAmount.value)) {
       const amountToBeReturned =
         Number(cashGiven.value) - Number(billAmount.value);
